ad5eb3d532132d6900974e94a98e71a030ff19b5
Sumanth-Uwin <pabbas@uwindsor.ca>
2026-07-29 23:15:52 -0400

    01-mongodb.sh

--- roboshop.sh
+++ roboshop.sh
@@ -7,7 +7,7 @@ for instance in $@
 do
     echo "Launching instance: $instance"
     INSTANCE_ID=$(aws ec2 run-instances \
-        --image-id $AMI_ID \
+        --image-id ami-0220d79f3f480ecf5 \
         --instance-type t3.micro \
         --security-groups "roboshop-common" "roboshop-$instance"\
         --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=roboshop-$instance}]" \
